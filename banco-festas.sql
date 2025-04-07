9c897cce6a83792da9cb3975160911f31d7f35f1
guhtorress <gu.paulogustavo@gmail.com>
2025-04-07 09:27:05 -0300

    Create banco-festas.sql

---
+++ banco-festas.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Clientes 
+( 
+); 
+
+CREATE TABLE Festas 
+( 
+); 
+
+CREATE TABLE Pagamento 
+( 
+); 
+
+CREATE TABLE Pix 
+( 
+ Código-Pix INT,  
+); 
+
+CREATE TABLE CartaoCredito 
+( 
+ Nome no cartao INT,  
+ Endereço INT,  
+ Cpf INT,  
+); 
+
+CREATE TABLE Boleto 
+( 
+ Data_vencimento INT,  
+ Cod_barras INT,  
+); 
